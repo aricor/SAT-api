81b553a5433e6d4be6bded877f4963c9a8939452
Vi <vivivi0701@gmail.com>
2019-11-04 05:22:12 +0100

    update button

--- src/Components/TestScreen.js
+++ src/Components/TestScreen.js
@@ -4,39 +4,57 @@ import Timer from './timer'
 import Highlight from './getHighlight'
 import ButtonComponent from './button'
 export default class TestScreen extends React.Component {
-    
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: "This passage is from James Joyce, The Dubliners originally published in 1914",
+            questions: [
+                {
+                    question: 
+                    "1. As presented in the passage, Mr. Kearney is best described as",  
+                    choice1: 
+                    "independent and strong willed.",
+                    
+                    
+                    choice2: 
+                     "paternal and affectionate.",  
+ 
+                    choice3: 
+                      "serious and family oriented.",  
+                
+                    choice4: 
+                        "boring and simple minded.",  
+               
+                }
+            ]
+        };
+    }
     render() {
-        const {testData, submitHandler} = this.props
-        return <div className="appContainer">
+        const {testData, submitHandler} = this.props;
+        const {questions} = this.state; 
+        return (
+        <div className="appContainer">
             <Timer/>            
             <div className="testContainer">
                 <div className="passageSection">
                 <div className="passagename">
-                <b>
-                This passage is from James Joyce, The Dubliners originally published in 1914                
-                </b>
+                <b>{this.state.title}</b>
                 </div>
                 <Highlight/>
                 </div>
                 <div className="questionSection">
                     <div className="article2">
-                    <ButtonComponent question="1. As presented in the passage, Mr. Kearney is best
-described as" choice1="independent and strong willed." choice2="paternal and aectionate." choice3="serious and family oriented." choice4="boring and simple minded." />
-
-                    <ButtonComponent question="1. As presented in the passage, Mr. Kearney is best
-                        described as" choice1="independent and strong willed." choice2="paternal and aectionate." choice3="serious and family oriented." choice4="boring and simple minded." />
-
-<ButtonComponent question="1. As presented in the passage, Mr. Kearney is best
-                        described as" choice1="independent and strong willed." choice2="paternal and aectionate." choice3="serious and family oriented." choice4="boring and simple minded." />
-
-<ButtonComponent question="1. As presented in the passage, Mr. Kearney is best
-                        described as" choice1="independent and strong willed." choice2="paternal and aectionate." choice3="serious and family oriented." choice4="boring and simple minded." />
-
-<ButtonComponent question="1. As presented in the passage, Mr. Kearney is best
-                        described as" choice1="independent and strong willed." choice2="paternal and aectionate." choice3="serious and family oriented." choice4="boring and simple minded." />
+                    <ButtonComponent 
+                    question={questions[0].question}
+                    choice1={questions[0].choice1}
+                    choice2={questions[0].choice2}
+                    choice3={questions[0].choice3}
+                    choice4={questions[0].choice4}
+                    />
                     </div>
                 </div>
             </div>
         </div>
+        )
     }
 }
--- src/Components/button.js
+++ src/Components/button.js
@@ -2,17 +2,19 @@ import React from "react";
 import { Button } from "reactstrap";
 import "./button.css";
 
-
 const ButtonComponent = props => {
   return (
     <div>
       <p><b> {props.question} </b></p>
       <div className="choice">
         <div className='mb-2' >
-          <Button className="btn-circle" outline color="primary">A</Button>{' '}
+          <div className="btn-circle"> 
+          <Button outline color="primary">A</Button>{' '}
           {props.choice1}
+          </div>
         </div>
       </div>
+      
       <div className="choice">
         <div className='mb-2' >
           <Button className="btn-circle" outline color="primary">B</Button>{' '}
