cdd517ff6bba28c1a13b5dfbab2264ec7ccbb272
Vi <vivivi0701@gmail.com>
2020-01-24 23:00:44 +0100

    update math section

--- src/Components/buttonMath.js
+++ src/Components/buttonMath.js
@@ -2,13 +2,20 @@ import React from "react";
 import {Button } from "reactstrap"; 
 import './button.css';
 
-const MathButton = () => {
-    <div style={styles}>
-    <Hello name="Form!" />
-    <form id="someForm" onSubmit={}>
-      <input name="ans" type="text" required />
-    </form>
-  </div>
+const MathButton = props => {
+    return (
+        <div>
+            <p><b> {props.question} </b></p>
+            <div>
+            <form id="someForm" on>
+            <input name="ans" type="text" required />
+            </form>
+           </div>
+        </div>
+
+    );
 }
+    
+
 
 export default MathButton; 
