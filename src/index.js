c8b2e2a8d7eefd61fa8b583c3c1bdd592a226bb1
Vi <vivivi0701@gmail.com>
2020-02-13 00:21:21 +0700

    Updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Home from './Components/testScreen';
-ReactDOM.render(
-  <Home/>,
-  document.getElementById('root')
-);
-
-module.hot.accept();
